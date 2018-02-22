364fcc298b02196b44b4ed7fa428f6981f9be377
Muriel <17842868+MurielSpot@users.noreply.github.com>
2018-02-22 16:24:53 +0800

    Update and rename 未完成A1010. Radix (25)#####.cpp to A1010. Radix (25)#####.cpp

--- A1010. Radix (25)#####.cpp
+++ A1010. Radix (25)#####.cpp
@@ -3,17 +3,153 @@ https://www.patest.cn/contests/pat-a-practise/1010
 
 类型：进制转换。二分查找。
 
-难度：5.
+难度：5.（很難全過）
 
 注意事项：
 1. 题意及其不清楚，所以要注意：
 基数radix的范围最大为INT_MAX，即2^31-1；
 已知进制的数默认对应的10进制不超过long long，而未知进制的数转换成10进制后可能是溢出的；
 2. 遍历进制数的方法会超时。所以可以通过二分查找来确定进制是多少。
-
-思考：
-假设数字大小超过long long，尝试用字符数组进行字符串的进制转换，或者定义结构体，用结构体的数组对数据进行进制转换。
+3. 進制範圍：做題時進制的下界可設為未知數（沒有確定基數的那個數）的每個位數中的最大值+1，上界設爲已知的那個數+1.
+4. 據説暴力遍历会在测试点7超时。 二分搜索后，如果不考虑溢出会在测试点10报错。
+5. 題中說可能有解不唯一的情況，擧個例子：2 2 1 4，答案為3到∞進制都是可以的，取最小解就應該是3.
 */
+
+//ac代碼。下面的代碼和這個幾乎一樣，衹是把所有的char數組換成了string，但不知道爲什麽出錯了，☹。（findLargestDigit略有改動，但我測試過了，僅替換這個函數是可以ac的。）
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+char n1[20], n2[20];
+int tag, radix;
+
+inline int char2ll(char c) {
+	if (isdigit(c)) {
+		return c - '0';
+	}
+	else return c - 'a' + 10;
+}
+
+long long convertToTen(char a[], long long radix) {
+	long long ans = 0;
+	int len = strlen(a);
+	for (int i = 0; i < len; i++) {
+		ans = ans * radix + char2ll(a[i]);
+	}
+	return ans;
+}
+
+int cmp(char n2[], long long radix, long long t) {
+	long long num = convertToTen(n2, radix);
+	if (num < 0) return 1;
+	if (num < t) return -1;
+	return (t == num) ? 0 : 1;
+}
+
+long long binarySearch(char n2[], long long left, long long right, long long t) {
+	long long mid;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		int flag = cmp(n2, mid, t);
+		if (flag == 0) return mid;
+		else if (flag == -1) left = mid + 1;
+		else right = mid - 1;
+	}
+	return -1;
+}
+
+int findLargestDigit(char n2[]) {
+	int ans = 2, len = strlen(n2);
+	for (int i = 0; i < len; i++) {
+		if (char2ll(n2[i]) >= ans) {
+			ans = char2ll(n2[i]) + 1;
+		}
+	}
+	return ans;
+}
+
+int main() {
+	scanf("%s%s%d%d", &n1, &n2, &tag, &radix);
+	if (tag == 2) {
+		swap(n1, n2);
+	}
+	long long t = convertToTen(n1, radix);
+	long long low = findLargestDigit(n2);
+	long long ans = binarySearch(n2, low, t + 1, t);
+	if (ans == -1) printf("Impossible");
+	else printf("%lld", ans);
+	return 0;
+}
+
+//0和6未過。
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include<string>
+using namespace std;
+string n1,n2;
+int tag, radix;
+
+inline int char2ll(char c){
+	if(isdigit(c)){
+		return c-'0';
+	}else return c-'a'+10;
+}
+
+long long convertToTen(string a, long long radix) {
+    long long ans = 0;
+    for(int i = 0; i < a.size(); i++) {
+        ans = ans * radix + char2ll(a[i]);
+    }
+    return ans;
+}
+
+int cmp(string s, long long radix, long long t) {
+    long long num = convertToTen(s, radix);
+    if(num < 0) return 1;
+    if(num < t) return -1;
+    return (t == num) ? 0 : 1;
+}
+
+long long binarySearch(string s, long long left, long long right, long long t) {
+    long long mid;
+    while(left <= right) {
+        mid = (left + right) / 2;
+        int flag = cmp(s, mid, t);
+        if(flag == 0) return mid;
+        else if(flag == -1) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int findLargestDigit(string s) {
+    int ans = -1;
+    for(int i = 0; i < s.size(); i++) {
+        if(char2ll(n2[i]) > ans) {
+            ans = char2ll(n2[i]);
+        }
+    }
+    return ans + 1;
+}
+
+int main() {
+    cin>>n1>>n2>>tag>>radix;
+	string a,b;
+    if(tag == 2) {
+        a=n2;
+		b=n1;
+    }else{
+		a=n1;
+		b=n2;
+	}
+    long long t = convertToTen(a, radix);
+    long long low = findLargestDigit(b);
+    long long ans = binarySearch(b, low, t+1, t);
+    if(ans == -1) printf("Impossible");
+    else printf("%lld", ans);
+    return 0;
+}
 
 
 //下面代碼有測試點0和6未過.
